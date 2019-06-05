8d9fecfae1b95c796782ea7af1466f37861360ee
Laila Chavez <lailamay.chavez@gmail.com>
2019-06-04 22:52:27 -0700

    fixed an error

--- app/src/main/java/com/abc/lailachavez/CameraList.java
+++ app/src/main/java/com/abc/lailachavez/CameraList.java
@@ -22,7 +22,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
+
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
