1891396e468a10f47c3775ff75f6633763e69793
Laila Chavez <lailamay.chavez@gmail.com>
2019-06-12 11:06:02 -0700

    created a java file for testing

--- app/src/main/java/com/abc/lailachavez/mockedSharedPreferences.java
+++ app/src/main/java/com/abc/lailachavez/mockedSharedPreferences.java
@@ -1,4 +1,29 @@
 package com.abc.lailachavez;
 
-public class MockedSharedPreferences {
+
+
+
+import android.content.SharedPreferences;
+
+import java.security.Key;
+
+public class mockedSharedPreferences {
+
+    private final SharedPreferences mockSharedPreferences;
+
+    private final String KEY = "input";
+
+    public mockedSharedPreferences(SharedPreferences sharedPreferences) {
+        mockSharedPreferences = sharedPreferences;
+    }
+
+    public boolean saveEntry(String entry) {
+        SharedPreferences.Editor mockEditor = mockSharedPreferences.edit();
+        mockEditor.putString(KEY, entry);
+        return mockEditor.commit();
+    }
+
+    public String getEntry() {
+        return mockSharedPreferences.getString(KEY, "");
+    }
 }
